249aea1b29dabcddcfa6f802600023a36153b89f
Sunnypyonk <sasha.pozniak@gmail.com>
2021-08-11 21:01:28 +0300

    Добавил виджет

--- Tk St.py
+++ Tk St.py
@@ -5,5 +5,13 @@ win.title('My app')
 photo = tk.PhotoImage(file='Logo.png')
 win.iconphoto(False, photo)
 win.geometry('800x600+500+200')
+win.config(bg='#8eb6ce')
+label = tk.Label(win, text='Привет',
+                 font=('Georgia', 11),
+                 padx=1, pady=1,
+                 anchor='n',
+                 relief=tk.RAISED,
+                 justify=tk.RIGHT)
+label.pack()
 
 win.mainloop()
